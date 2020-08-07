e11f8af3089b4e16a69bb67ac25a39373abd63ea
hyeji1221 <lhg2615@naver.com>
2020-08-07 13:12:21 +0900

    post창 버튼 이동

---
+++ app/src/main/java/com/example/myapplication/Post.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Post extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hyeji_post);
+        findViewById(R.id.postadd).setOnClickListener(onClickListener);
+        findViewById(R.id.mypagebtn).setOnClickListener(onClickListener);
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            switch(v.getId()){
+                case R.id.postadd:
+                    Intent intent_post = new Intent(v.getContext(), SignUpExample.class); //바꾸기 (사진 추가하는 창으로)
+                    startActivity(intent_post);
+                    break;
+                case R.id.mypagebtn:
+                    Intent intent_mypage = new Intent(v.getContext(), Signup.class); //바꾸기 (mypage 창으로)
+                    startActivity(intent_mypage);
+                    break;
+            }
+        }
+
+    };
+}
