669a0ea5a755e26504f10c6dbefe39ce94e7d285
sja3410 <sja3410@gamil.com>
2020-08-14 11:28:03 +0900

    gallery 불러오기

--- app/src/main/java/com/example/myapplication/Add_gallery.java
+++ app/src/main/java/com/example/myapplication/Add_gallery.java
@@ -2,7 +2,6 @@ package com.example.myapplication;
 
 import android.Manifest;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -22,41 +21,59 @@ import androidx.core.content.ContextCompat;
 
 import java.io.InputStream;
 
+
 public class Add_gallery extends AppCompatActivity {
     ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.add_gallery);
-    checkSelfPermission();
-    iv = findViewById(R.id.gallery_image);
-    iv.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent();
-            intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
-            intent.setAction(Intent.ACTION_GET_CONTENT);
-            startActivityForResult(intent,101);
-        }
-    });
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_gallery);
+        checkSelfPermission();
+        iv = findViewById(R.id.gallery_image);
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 101);
+            }
+        });
 
     }
 
-    public void checkSelfPermission() {
-        String temp="";
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            temp+= Manifest.permission.READ_EXTERNAL_STORAGE+" ";
+    // 권한에 대한 응답이 있을 때 작동하는 함수
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) { // 권한을 허용했을 경우
+            int length = permissions.length;
+            for (int i = 0; i<length; i++) {
+                if(grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    // 동의
+                    Log.d("add_gallery","권한 허용: "+permissions[i]);
+                }
+            }
         }
-        if (ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED) {
-            temp += Manifest.permission.WRITE_EXTERNAL_STORAGE+ " ";
+    }
+
+
+    public void checkSelfPermission() {
+        String temp = "";
+        // 파일 읽기 권한 확인
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.READ_EXTERNAL_STORAGE + " ";
+        }
+        // 파일 쓰기 권한 확인
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.WRITE_EXTERNAL_STORAGE + " ";
         }
         if (TextUtils.isEmpty(temp) == false) { // 권한 요청
-            ActivityCompat.requestPermissions(this,temp.trim().split(" "),1);
-        }
-        else { // 모두 허용 상태
+            ActivityCompat.requestPermissions(this, temp.trim().split(" "), 1);
+        } else { // 모두 허용 상태
             Toast.makeText(this, "권한을 모두 허용", Toast.LENGTH_SHORT).show();
         }
+
 
     }
     @Override
