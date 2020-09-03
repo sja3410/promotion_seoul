f526d3a5abd94a6057a3545cfe3dcbc11981b1cd
LAH1203 <lah1203@naver.com>
2020-09-03 16:11:25 +0900

    사용자 연동 제외 검색기능 구현완료

--- app/src/main/java/com/example/myapplication/search.java
+++ app/src/main/java/com/example/myapplication/search.java
@@ -16,33 +16,43 @@ import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class search extends AppCompatActivity {
 
     private EditText search_edittext;
-    private ListView s_listview;
-    private ScrollView s_scrollview;
-
-    // 터치이벤트를 통해 리스트뷰가 스크롤될 수 있게 함
-    private void init(){
-        s_scrollview = (ScrollView)findViewById(R.id.search_scroll_view);
-        s_listview = (ListView)findViewById(R.id.search_list_view);
-
-        s_listview.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                s_scrollview.requestDisallowInterceptTouchEvent(true);
-                return false;
-            }
-        });
-    }
+    private List<String> list;  // 데이터를 넣을 리스트변수
+    private ListView search_listView;  // 검색을 보여줄 리스트변수
+    private SearchAdapter adapter;  // 리스트뷰에 연결할 어댑터
+    private ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        search_edittext = findViewById(R.id.search_edittext);
+        search_edittext = (EditText) findViewById(R.id.search_edittext);
+        search_listView = (ListView) findViewById(R.id.search_list_view);
+
+        // 리스트 생성
+        list = new ArrayList<String>();
+
+        // 데이터베이스에서 사용자의 정보 가져와서 저장
+        settingList();
+
+        // 리스트의 모든 데이터를 arrayList에 복사
+        arrayList = new ArrayList<String>();
+        arrayList.addAll(list);
+
+        // 리스트에 연동할 어댑터 생성
+        adapter = new SearchAdapter(list, this);
+
+        // 리스트뷰에 어댑터 연결
+        search_listView.setAdapter(adapter);
+
+        // 검색어 입력 이벤트 리스너
         search_edittext.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -57,9 +67,32 @@ public class search extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable editable) {
                 String searchText = search_edittext.getText().toString();
-                // adapter.filter(searchText);  <- 이제 다음 시작은 어댑터 추가부터(커스텀뷰 제작 참고)
+                search_person(searchText);
             }
         });
+    }
+
+    public void search_person(String Text){
+        // 입력 초기화
+        list.clear();
+
+        if(Text.length() == 0){
+            list.addAll(arrayList);
+        } else {
+            for (int i = 0; i < arrayList.size(); i++) {
+                if (arrayList.get(i).toLowerCase().contains(Text)) {
+                    list.add(arrayList.get(i));
+                }
+            }
+        }
+        // 어댑터 갱신
+        adapter.notifyDataSetChanged();
+    }
+
+    private void settingList() {
+        list.add("안선정");
+        list.add("이아현");
+        list.add("임혜지");
     }
 }
 
