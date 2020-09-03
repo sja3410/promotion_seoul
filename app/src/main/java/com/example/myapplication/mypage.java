9d3d3cb2accc9af73477cc35af82cc324a7afd41
hyeji1221 <lhg2615@naver.com>
2020-09-03 18:28:29 +0900

    테이블 레이아웃, 마이페이지 username 불러오기

--- app/src/main/java/com/example/myapplication/mypage.java
+++ app/src/main/java/com/example/myapplication/mypage.java
@@ -3,15 +3,34 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class mypage extends AppCompatActivity {
+    TextView user_name;
+    TextView user_follower;
+    TextView user_posting;
+    TextView user_following;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    DocumentReference docRef = db.collection("Profile").document(user.getUid());
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage);
         findViewById(R.id.profile_change).setOnClickListener(onClickListener);
+        getUsername();
     }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -25,4 +44,18 @@ public class mypage extends AppCompatActivity {
             }
         }
     };
+    public void getUsername(){
+        user_name = findViewById(R.id.user);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>(){
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                DocumentSnapshot document = task.getResult();
+                user_name.setText(document.getString("username"));
+            }
+        });
+    }
+
+
+
+
 }
