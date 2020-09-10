b59eaa803cc7a4c0c7767a287b361719622498b2
hyeji1221 <lhg2615@naver.com>
2020-09-11 00:28:20 +0900

    사용자 정보 세팅 수정

--- app/src/main/java/com/example/myapplication/mypage.java
+++ app/src/main/java/com/example/myapplication/mypage.java
@@ -12,29 +12,35 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Map;
 
 public class mypage extends AppCompatActivity {
     TextView user_name;
+    TextView write_user;
     TextView user_follower;
     TextView user_posting;
     TextView user_following;
-    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+   /* FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    DocumentReference docRef = db.collection("Profile").document(user.getUid());
+    DocumentReference docRef = db.collection("Profile").document(user.getUid());*/
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference usersCollectionRef = db.collection("Profile");
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage);
         findViewById(R.id.profile_change).setOnClickListener(onClickListener);
+        user_name = findViewById(R.id.user);
+        write_user = findViewById(R.id.write_username);
         getUsername();
-        get_post();
-        get_follower();
-        get_following();
     }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -42,24 +48,39 @@ public class mypage extends AppCompatActivity {
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.profile_change:
-                    Intent intent_picture = new Intent(v.getContext(),profile_picture.class);
+                    Intent intent_picture = new Intent(v.getContext(), profile_picture.class);
                     startActivity(intent_picture);
                     break;
             }
         }
     };
+
     public void getUsername(){
         user_name = findViewById(R.id.user);
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>(){
+        write_user = findViewById(R.id.write_username);
+        user_posting = findViewById(R.id.gesi);
+        user_follower = findViewById(R.id.follower);
+        user_following = findViewById(R.id.following);
+        usersCollectionRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                DocumentSnapshot document = task.getResult();
-                user_name.setText(document.getString("username"));
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    Map map = document.getData();
+                    String user = map.get("username").toString();
+                    String post = map.get("posting").toString();
+                    String follower = map.get("follower").toString();
+                    String following = map.get("following").toString();
+                    user_name.setText(user);
+                    write_user.setText(user);
+                    user_posting.setText(post +"\n"+ "게시물");
+                    user_follower.setText(follower+"\n"+"팔로워");
+                    user_following.setText(following + "\n" + "팔로잉");
+                }
             }
         });
     }
 
-    public void get_post() {
+   /* public void get_post() {
         user_posting = findViewById(R.id.gesi);
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>(){
             @Override
@@ -95,7 +116,7 @@ public class mypage extends AppCompatActivity {
                 user_following.setText("팔로잉\n"+following.toString());
             }
         });
-    }
+    }*/
 
 
 }
